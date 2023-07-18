9133d4e8c8f27b590222d2d1b37e1d604728e602
Manuel Quispe Cueva <90989286+ManuQC21@users.noreply.github.com>
2023-07-17 23:15:14 -0500

    Add files via upload

---
+++ en/java.js
@@ -0,0 +1,11 @@
+var check=document.querySelector(".check");
+check.addEventListener('click',idioma);
+
+function idioma(){
+    let id=check.checked;
+    if (id==false){
+        location.href="../indexen.html";
+    }else{
+        location.href="../indexen.html";
+    }
+}
